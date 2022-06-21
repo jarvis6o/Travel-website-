3bb54c45fe1245f4ae9bd55f82d6c96836bcf72c
jarvis6o <75544184+jarvis6o@users.noreply.github.com>
2022-06-21 23:10:26 +0530

    Add files via upload

---
+++ travel/bottom.php
@@ -0,0 +1,49 @@
+<div class="fotter">
+	 <div class="container">
+		 <div class="fotter-grids">
+			 <div class="col-md-4 fotter-left">
+			 <p style="text-align:justify"></p>
+			 </div>
+			 <div class="col-md-4 fotter-middle">
+				 <h3></h3>
+				 <div class="footer-list">
+						<h3></h3>
+                        <div class="footer-list">
+                        </div>
+
+			     </div>
+			 <div class="col-md-4 fotter-right" style="padding-left:10px">
+			 <h3></h3><br/>
+              <div class="footer-list">
+			  </div></div>
+
+				<section class="footer">
+ 			     <div class="box-container">
+
+ 			         <div class="box">
+ 			             <h3>contact info</h3>
+ 			             <a href="#"><i class="fas fa-phone"></i>+91 73398 14571</a>
+ 			             <a href="#"><i class="fas fa-phone"></i>+91 93193 16601</a>
+ 			             <a href="#"><i class="fas fa-envelope"></i>smilenaturally9@gmail.com</a>
+ 			             <a href="#"><i class="fas fa-map"></i>Rajasthan, India - 302018</a>
+ 			         </div>
+
+
+ 			 </section>
+
+			 <div class="social-icons">
+			 <a href="https://www.facebook.com/profile.php?id=100057607206565"><span class="facebook"> </span></a>
+			 <a href="https://twitter.com/Satyam15242239?t=XrNaZ80HfSiUqnJTJd9eDg&s=08"><span class="twitter"> </span></a>
+			 <a href="#"><span class="googleplus"> </span></a>
+			 <a href="https://www.linkedin.com/in/sushil-kumar-115721210"><span class="pinterest"> </span>linkedin</a>
+			 <a href="https://instagram.com/___sameer__2407?utm_medium=copy_link"><span class="instagram"> </span></a>
+			 </div>
+			 <div class="clearfix"></div>
+	     </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+</div>
+<div class="credit">Copyright © 2020 <span></span> || all rights reserved</div>
+<p style="padding-right:100px"></p>
+</div>
